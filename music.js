d2d0ed9f00910adae5adc0fff94c79e1a86672a1
kanepasu <102670872+kanepasu@users.noreply.github.com>
2023-02-25 21:42:24 +0900

    Update music.js

--- music.js
+++ music.js
@@ -1,11 +1,12 @@
 function OnButtonClick() {
     let element = document.getElementById("floatingInput");
     full_pass = element.value;
+    true_pass = "file://"+full_pass;
     return{
-        full_pass
+        true_pass
     };
 }
 function OnButtonClick2() {
-    let music = new Audio(OnButtonClick().full_pass);
+    let music = new Audio(OnButtonClick().true_pass);
     music.play();
 }
